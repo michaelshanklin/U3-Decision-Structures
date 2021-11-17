1fedd0930757c53fb7407b275f89f0852abc6b55
Michael Shanklin <shankmic002@warren.k12.in.us>
2021-11-17 12:21:12 -0500

    L2

--- L2-If Else Statements/src/Prom.java
+++ L2-If Else Statements/src/Prom.java
@@ -6,4 +6,31 @@ public class Prom {
      */
 
 
+    public static void main(String[] args) {
+
+        int gradeLevel = Integer.parseInt(JOptionPane.showInputDialog("What grade level are you in?"));
+        int disciplinary = Integer.parseInt(JOptionPane.showInputDialog("How many suspensions have you had?"));
+        int money = Integer.parseInt(JOptionPane.showInputDialog("How much money do you have? Round down to the nearest $5"));
+
+        if(grade level >= 11){
+
+            if(disciplinary >= 1){
+                System.out.printn("You can't go to prom. Be a good student.");
+            }else {
+
+                if(money is >= 40){
+                    System.out.println("You can get a prom ticket");
+                }else{
+                    System.out.println("You can't go to prom");
+                }
+
+            }
+
+        }else{
+
+            System.out.println("You can't go to prom.");
+
+        }
+
+    }
 }
--- L2-If Else Statements/src/ShippingCosts.java
+++ L2-If Else Statements/src/ShippingCosts.java
@@ -21,6 +21,36 @@ public class ShippingCosts {
 
     As always, your program should include a method.
      */
+    public static void main(String[] args) {
 
+        double shippingRate = Double.parseDouble(JOptionPane.showInputDialog("What is the weight of this item?"));
+        int milesShipped = Integer.parseDouble(JOptionPane.showInputDialog("How many miles are you shipping it?"));
+        double itemWeight;
+        double cost;
 
+        if(item weight >= 10){
+            shippingRate = 4.80;
+        }else{
+           if(itemWeight >= 6){
+               shippingRate = 3.70;
+           }else{
+               if(itemWeight = 2){
+                   shippingRate = 2.20;
+               }else{
+                   shippingRate = 1.10;
+               }
+           }
+    }
+
+    public static double cost(double rate, int miles) {
+
+        int costMultiplier = miles / 100 + 1;
+
+        if (costMultiplier == 0) {
+            costMultiplier = 1;
+
+        }
+
+        return rate * costMultiplier;
+    }
 }
